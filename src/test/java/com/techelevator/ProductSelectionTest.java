5d98c9d0e3ec83756ad8f5c36a38424d96e730c8
Ashton <guntowksi@gmail.com>
2022-12-13 21:56:08 -0600

    Product Selection committed, need to change placeholder

---
+++ src/test/java/com/techelevator/ProductSelectionTest.java
@@ -0,0 +1,18 @@
+package com.techelevator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ProductSelectionTest {
+
+    @Test
+    public void ProductSelection(){
+        ProductSelection ps = new ProductSelection();
+        ps.fillProduct();
+        ps.userInput = "A2";
+        //ps.stock = 0;
+        ps.selectProduct(ps.userInput);
+        //Assert.assertEquals(ps.dispenseItem(testCode));
+    }
+
+}
